6810eac6382b36ad42e9b8043175c2f2f8c82d1e
Hiroyuki Okamura <okamumu@gmail.com>
2017-02-11 07:38:37 +0900

    add several distributions

--- src/jp/rel/jmtrandom/Random.java
+++ src/jp/rel/jmtrandom/Random.java
@@ -19,6 +19,16 @@ public class Random {
 		return mtrand.genRandRealo0o1();
 	}
 
+	public double nextUnif(double low, double high) {
+		return (high - low) *  mtrand.genRandRealo0o1() + low;
+	}
+
+	public double nextExp(double beta) {
+		return -Math.log(this.nextUnif()) / beta;
+	}
+
+	/* Normal distribution */
+
 	public double nextNormal() {
 		if (normal_sw == 0) {
 			double t = Math.sqrt(-2 * Math.log(this.nextUnif()));
@@ -36,8 +46,49 @@ public class Random {
 		return mu + sig * this.nextNormal();
 	}
 
-	public double nextExp(double r) {
-		return -Math.log(this.nextUnif()) / r;
+	public double nextTruncNormal(double mu, double sig) {
+		double tmp;
+		do {
+			tmp = this.nextNormal(mu, sig);
+		} while (tmp > 0.0);
+		return tmp;
+	}
+
+	public double nextLogNormal(double mu, double sig) {
+		return Math.exp(nextNormal(mu, sig));
+	}
+
+	/* Logistic distribution */
+
+	public double nextLogistic() {
+    	double x = this.nextUnif();
+    	return Math.log(x/(1.0-x));
+    }
+
+    public double nextLogistic(double mu, double phi) {
+		return mu + phi * this.nextLogistic();
+    }
+
+    public double nextTruncLogistic(double mu, double phi) {
+		double tmp;
+		do {
+			tmp = this.nextLogistic(mu, phi);
+		} while (tmp > 0.0);
+		return tmp;
+    }
+
+	public double nextLogLogistic(double mu, double phi) {
+		return Math.exp(nextLogistic(mu, phi));
+	}
+	
+	/* Extreme distribution */
+
+	// TODO
+	
+	/* Weibull distribution */
+
+	public double nextWeibull(double alpha, double beta) {
+		return Math.pow(-Math.log(this.nextUnif()) / beta, 1.0/alpha);
 	}
 
 }
